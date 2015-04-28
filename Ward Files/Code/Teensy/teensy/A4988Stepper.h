1369a272530b55efa0a0a3feb128f3e18018368c
Ward Prescott <erp006@bucknell.edu>
2015-04-28 00:15:35 -0400

    Cleaning.

---
+++ Ward Files/Code/Teensy/teensy/A4988Stepper.h
@@ -0,0 +1,44 @@
+#ifndef GUARD1
+#define GUARD1
+
+#define UP 1
+#define DOWN -1
+
+class Stepper
+{
+public:
+  //Constructor
+  Stepper(int, int, int, int, int, void(*)());
+  void setDir(int);
+  void enable();
+  void disable();
+  void calibrate();
+  void step(int);
+  void test();
+  int  getPosition();
+  void toggleDir();
+
+  int counter;
+  int direction;
+  int status;
+  int upperBound;
+  int lowerBound;
+  void (*refresh)();
+  void (*asyncFunc)();
+  int PotPin;
+  int DesiredPosition;
+  int RampUp;
+  int RampDn;
+  int DirPin;
+    int StepPin;
+
+private:
+  int ENPin;
+
+
+  int LimitSwitchPin;
+  
+};
+
+#endif
+
